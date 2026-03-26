8094402704b3f4522f51f8c3cba1ff5aeaf237a7
youcode safi <tajat.nourelhouda@gmail.com>
2026-03-26 14:53:03 +0100

    fix(models): remove duplicate HasApiTokens import in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-use Laravel\Sanctum\HasApiTokens;
+
 
 #[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
