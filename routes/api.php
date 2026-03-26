63cf8e4a30280ccfceaf81980dc181208635a60a
𝚂𝙰𝙷𝙷𝙾𝚄𝚃𝙸 𝙰𝙼𝙸𝙽𝙴 <midoriaduko@gmail.com>
2026-03-26 10:00:06 +0100

    feat(routes): add auth and tournament registration API routes

--- routes/api.php
+++ routes/api.php
@@ -8,14 +8,29 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 use App\Http\Controllers\Api\TournamentController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TournamentRegistrationController;
+
+// Auth Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 // player routes
 Route::get('tournaments', [TournamentController::class, 'index']);
 Route::get('tournaments/{tournament}', [TournamentController::class, 'show']);
 
-// organizer routes
 Route::middleware('auth:sanctum')->group(function () {
+    // Logout
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Tournament organizer routes
     Route::post('tournaments', [TournamentController::class, 'store']);
     Route::put('tournaments/{tournament}', [TournamentController::class, 'update']);
     Route::delete('tournaments/{tournament}', [TournamentController::class, 'destroy']);
+    
+    // Organizer retrieving participants
+    Route::get('tournaments/{tournament}/participants', [TournamentRegistrationController::class, 'participants']);
+
+    // Player registering for tournament
+    Route::post('tournaments/{tournament}/register', [TournamentRegistrationController::class, 'register']);
 });
