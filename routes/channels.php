03936e649ebd98305168c5e1f5bc3edea375ec90
𝚂𝙰𝙷𝙷𝙾𝚄𝚃𝙸 𝙰𝙼𝙸𝙽𝙴 <midoriaduko@gmail.com>
2026-03-27 15:40:32 +0100

    feat(channels): register public tournament broadcast channel

--- routes/channels.php
+++ routes/channels.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('tournament.{id}', function ($user = null, $id) {
+    // Public channel: anyone can listen, even if not logged in.
+    return true;
+});
